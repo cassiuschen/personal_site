5059221e65e10ec25866be95960f415e7996913e
cassiuschen <chzsh1995@gmail.com>
2015-04-14 14:53:17 +0800

    finish post show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'home/index'
+  root 'home#index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
